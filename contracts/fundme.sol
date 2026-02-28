6f306d38a8f940a63f3df145e68222733cacd814
akkidakki909 <akshattripathi1611@gmail.com>
2026-02-28 12:35:07 +0530

    p

--- contracts/fundme.sol
+++ contracts/fundme.sol
@@ -10,4 +10,15 @@ contract fundme{
         //what is reverting?
         //undo any action before, and send remaining gas back 
     }
-}
+
+    function getPrice() public{
+        //ABI
+        //Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+    }
+    
+    //function getVersion() public view returns (uint256){
+      // AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+      // return priceFeed.version();
+   // }
+        function getConversionRate()public{}
+    }
